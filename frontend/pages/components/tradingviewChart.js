272541e40b0b6251d007d0e10284faed9798ef2d
seongjun <skim477@myseneca.ca>
2024-09-30 06:11:56 -0400

    improve error handling of tradingview chart

--- frontend/pages/components/tradingviewChart.js
+++ frontend/pages/components/tradingviewChart.js
@@ -1,42 +1,62 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Container, Alert } from 'react-bootstrap';
 
 const TradingViewChart = ({symbol}) => {
+  const [error, setError] = useState(null);
   const containerRef = useRef(null);
 
   useEffect(() => {
-    if (containerRef.current) {
-      const script = document.createElement('script');
-      script.src = 'https://s3.tradingview.com/tv.js';
-      script.async = true;
+    try {
+          if (containerRef.current) {
+            const script = document.createElement('script');
+            script.src = 'https://s3.tradingview.com/tv.js';
+            script.async = true;
 
-      script.onload = () => {
-        new window.TradingView.widget({
-          container_id: containerRef.current.id,
-          autosize: false,
-          symbol: symbol, // e.g., 'AAPL'
-          interval: 'D',
-          timezone: 'Etc/UTC',
-          theme: 'light',
-          style: '1',
-          locale: 'en',
-          toolbar_bg: '#f1f3f6',
-          enable_publishing: false,
-          allow_symbol_change: true,
-          save_image: false,
-          details: true,
-          width: "100%",
-          height: "500px",
-        });
-      };
+            script.onload = () => {
+              try {
+                new window.TradingView.widget({
+                  container_id: containerRef.current.id,
+                  autosize: false,
+                  symbol: symbol, // e.g., 'AAPL'
+                  interval: 'D',
+                  timezone: 'Etc/UTC',
+                  theme: 'light',
+                  style: '1',
+                  locale: 'en',
+                  toolbar_bg: '#f1f3f6',
+                  enable_publishing: false,
+                  allow_symbol_change: true,
+                  save_image: false,
+                  details: true,
+                  width: "100%",
+                  height: "500px",
+                });
+              } catch (error) {
+                setError('Failed to initialize TradingView widget')
+                console.error(error);
+              }
+
+            };
 
       containerRef.current.appendChild(script);
     }
+    } catch (error) {
+      setError(error.message);
+      console.error(error);
+    }
+
   }, [symbol]);
 
   return (
-    <div className="tradingview-widget-container" >
-      <div id="tradingview_chart" ref={containerRef}></div>
-    </div>
+    <Container>
+      {error ? (
+        <Alert variant='danger'>{error}</Alert>
+      ) : (
+        <div className="tradingview-widget-container" >
+          <div id="tradingview_chart" ref={containerRef}></div>
+        </div>
+      )}
+    </Container>
   );
 };
 
